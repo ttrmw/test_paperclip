d6b46406ee8e50edb879e00ab48240b84b5b58eb
Thomas Millward Wright <thomas@ttrmw.co.uk>
2015-04-14 23:46:40 +0100

    add setup tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,54 @@
+require 'rake'
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do 
+    User.has_thumb = false 
+  end 
+
+  test 'can create dummy user' do 
+    assert_difference 'User.all.count', 1 do 
+      User.create_dummy_user!
+    end
+  end 
+
+  test 'dummy user is created with avatar' do 
+    user = User.create_dummy_user!
+    assert_not_nil user.avatar_file_name
+  end 
+
+  test 'can create one hundred dummy users' do 
+    assert_difference 'User.all.count', 100 do 
+      User.create_one_hundred_dummy_users!
+    end
+  end
+
+  test 'can create five hundred dummy users' do 
+    assert_difference 'User.all.count', 500 do 
+      User.create_five_hundred_dummy_users! 
+    end 
+  end
+
+  test 'Has thumb defaults to false' do 
+    assert_equal false, User.has_thumb?
+  end
+
+  test 'Has thumb can be updated' do
+    assert_equal false, User.has_thumb? 
+    User.has_thumb = true 
+    assert_equal true, User.has_thumb?
+  end
+
+  test 'Style returns nil if User does not have thumb' do 
+    assert_equal false, User.has_thumb?
+    assert_equal( {}, User.paperclip_styles ) 
+  end
+
+  test 'Style returns thumb if User has thumb' do 
+    User.has_thumb = true 
+    assert_equal( { thumb: '100x100>' }, User.paperclip_styles )
+  end
+
 end
+
+
